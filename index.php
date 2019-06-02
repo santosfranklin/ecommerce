2f1c97ed6e6442a69b9ecaf4730a3399bf16913e
santosfranklin <santosfranklinsilva@gmail.com>
2019-06-02 02:19:28 -0300

    inclusao da classe DB

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
@@ -8,13 +8,11 @@ $app = new Slim();
 
 $app->config('debug', true);
 
-require_once("functions.php");
-require_once("site.php");
-require_once("admin.php");
-require_once("admin-users.php");
-require_once("admin-categories.php");
-require_once("admin-products.php");
-require_once("admin-orders.php");
+$sql = new Hcode\DB\Sql();
+
+$results = $sql->select("SELECT * FROM tb_users");
+
+echo json_encode($results);
 
 $app->run();
 
